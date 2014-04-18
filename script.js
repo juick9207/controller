d66dfa53280f908e1f432d3f1564e4f08f63f631
juick9207 <juick9207@163.com>
2014-04-18 16:09:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$(document).ready (function(){
+$(document).ready(function(){
 
-	$("#up-btn").click(function(){
+	$('up-btn').click(function(){
 		$('#player').animate({top:'-=50px'},300);
 	});
-	$("#down-btn").click(function(){
-		$('#player').animate({top:'+=50px'}300);
+	$('#down-btn').click(function(){
+		$('#player').animate({top:'+=50px'},300);
 	});
 	$("#left-btn").click(function(){
-		$('#player').animate({left:'-=50px'}300);
+		$('#player').animate({left:'-=50px'},300);
 	});
 	$('#right-btn').click(function(){
-		$('#player').animate({left:'+=50px'}300);
+		$('#player').animate({left:'+=50px'},300);
 	});
 	$("#in-btn").click(function(){
 		$('#player').fadeIn('slow');
